c900a4278c30737763751ba795f49dbc2f8af910
maple3142 <kirby741852963@gmail.com>
2024-05-12 11:25:46 +0800

    v3.2.0

--- download.sh
+++ download.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-VERSION="3.1.12.1"
+VERSION="3.2"
 URL="https://github.com/jgm/pandoc/releases/download/$VERSION/pandoc-$VERSION-linux-amd64.tar.gz"
 curl "$URL" -Lo pandoc.tar.gz
 tar -xf pandoc.tar.gz "pandoc-$VERSION/bin/pandoc" --strip-components=2
